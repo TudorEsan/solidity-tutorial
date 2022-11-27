6930dddf22da752e9dfe988b789e0737a069a705
Tudor <tudor.esan@icloud.com>
2022-11-27 21:08:41 +0200

    compile Inbox.sol

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -2,3 +2,19 @@ pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
+
+contract Inbox {
+  string message;
+
+  function Inbox(string initialMessage) public {
+    message = initialMessage;
+  }
+  
+  function getMessage() public view returns (string) {
+    return message;
+  }
+
+  function setMessage(string newMessage) public {
+    message = newMessage;
+  }
+}
